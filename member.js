9fa783aa6d8fcd068c1d96104752c7626915a441
ellamfoley <124301466+ellamfoley@users.noreply.github.com>
2023-09-14 17:34:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,27 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberDiv = document.getElementById("memberDiv");
+    var skillsDiv = document.getElementById("skillsDiv");
+    var projectsDiv = document.getElementById("projectsDiv");
+    var contactDiv = document.getElementById("contactDiv");
+    var aboutDiv = document.getElementById("aboutDiv");
+    member.style.backgroundColor = "#ff5c5c";
+    skills.style.backgroundColor = "#ff5c5c";
+    projects.style.backgroundColor = "#ff5c5c";
+    contact.style.backgroundColor = "#ff5c5c";
+    about.style.backgroundColor = "#ff5c5c";
+    member.style.color = "#fff";
+    skills.style.color = "#fff";
+    projects.style.color = "#fff";
+    contact.style.color = "#fff";
+    about.style.color = "#fff";
+    memberDiv.style.display = "block";
+    skillsDiv.style.display = "none";
+    projectsDiv.style.display = "none";
+    contactDiv.style.display = "none";
+    aboutDiv.style.display = "none";
+}
